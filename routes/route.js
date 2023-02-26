6e84b8e613ae6c0afdae4b99dd3f7f29913dd118
LNagad <nagadgames100k@gmail.com>
2023-02-26 14:25:49 -0400

    debug nodemon config added

--- routes/route.js
+++ routes/route.js
@@ -15,18 +15,19 @@ const requestHandler = function (req, res) {
         "/skills": [skillsPage, "skills"],
         "/projects": [projectsPage, "projects"],
         "/contact": [contactPage, "projects"]
-      };
+    };
       
-      if (pages.hasOwnProperty(requestURL)) {
+    if (pages.hasOwnProperty(requestURL)) {
         res.setHeader("content-type", "text/html");
         res.write(masterPage(pages[requestURL][0], pages[requestURL][1]));
         res.statusCode = 200
         res.end()
+
     } else {
         res.write(masterPage(notFoundpage, "404"));
         res.statusCode = 400
         res.end()
-      }
+    }
   
 }
 
